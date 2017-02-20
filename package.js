9e7f226253eaf0ee92106f98bcb65e8f568b12a2
Fizer Khan <fizerkhan@gmail.com>
2017-02-20 22:08:23 +0530

    Removed amd support.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: 'Integrate with Atatus JS for JavaScript errors and performance metrics',
-  version: '0.0.5',
+  version: '0.0.6',
   name: 'atatus:atatus',
   git: 'https://github.com/atatus/atatus-meteor.git'
 });
